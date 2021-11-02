06cb704fd4efbb2350802de4bdf59fd50dab353c
Malachi <malachi@mackie.co.nz>
2021-11-02 22:17:26 +1300

    Add gizmo for enemy spawn

--- Assets/Scripts/Core/EnemySpawn.cs
+++ Assets/Scripts/Core/EnemySpawn.cs
@@ -1,4 +1,6 @@
+using System;
 using Gameplay;
+using UnityEditor;
 using UnityEngine;
 
 namespace Core
@@ -6,5 +8,11 @@ namespace Core
     public class EnemySpawn : MonoBehaviour
     {
         public Enemy.EnemyType type;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.white;
+            Gizmos.DrawIcon(transform.position, "Enemy");
+        }
     }
 }
