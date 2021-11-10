8a152132850c64d87bfac7af948b3855cac37eaa
Malachi <malachi@mackie.co.nz>
2021-11-10 20:18:13 +1300

    Multiple updates
    - Add Player Spawn Gizmo
    - Allow collecting collectables after player reached end
    - Update Level 2 Layout

---
+++ Assets/Scripts/Core/Spawns/PlayerSpawn.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Core.Spawns
+{
+    public class PlayerSpawn : MonoBehaviour
+    {
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawIcon(transform.position, "Start");
+        }
+    }
+}
--- Assets/Scripts/Core/UI/LevelEndScreen.cs
+++ Assets/Scripts/Core/UI/LevelEndScreen.cs
@@ -15,7 +15,8 @@ namespace Core.UI
         IRequireSetup,
         IPlayerReachedFinishEventTarget,
         INextLevelStartedEventTarget,
-        ILevelRestartedEventTarget
+        ILevelRestartedEventTarget,
+        ICollectedCollectablesChangedEventTarget
     {
         [SerializeField] private Text collectablesText;
         [SerializeField] private string collectablesTextFormat;
@@ -25,18 +26,27 @@ namespace Core.UI
 
         [SerializeField] private Transform button1Position;
         [SerializeField] private Transform button2Position;
+
+        private int _totalCollectables;
         
         public void Setup()
         {
             gameObject.SetActive(false);
             var anyMoreLevels = false;
             Helpers.DispatchEvent<IRequestAnyMoreLevelsEventTarget>(x => x.RequestAnyMoreLevels(out anyMoreLevels));
+            Helpers.DispatchEvent<IRequestTotalCollectablesEventTarget>(x =>
+                x.RequestTotalCollectables(out _totalCollectables));
             if (!anyMoreLevels)
             {
                 nextLevelButton.gameObject.SetActive(false);
                 restartLevelButton.transform.position = button1Position.transform.position;
                 quitButton.transform.position = button2Position.transform.position;
             }
+        }
+
+        public void CollectedCollectablesChanged(int collected)
+        {
+            collectablesText.text = string.Format(collectablesTextFormat, collected, _totalCollectables);
         }
 
         public void NextLevel()
@@ -57,13 +67,10 @@ namespace Core.UI
         public void PlayerReachedFinish()
         {
             gameObject.SetActive(true);
-            var totalCollectables = 0;
             var collectedCollectables = 0;
-            Helpers.DispatchEvent<IRequestTotalCollectablesEventTarget>(x =>
-                x.RequestTotalCollectables(out totalCollectables));
             Helpers.DispatchEvent<IRequestCollectedCollectablesEventTarget>(x =>
                 x.RequestCollectedCollectables(out collectedCollectables));
-            collectablesText.text = string.Format(collectablesTextFormat, collectedCollectables, totalCollectables);
+            collectablesText.text = string.Format(collectablesTextFormat, collectedCollectables, _totalCollectables);
         }
 
         public void NextLevelStarted()
